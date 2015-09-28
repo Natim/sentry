b0b8402cfe15c902312c57b2656799fa9918300a
Matt Robenolt <matt@ydekproductions.com>
2015-09-28 15:54:21 -0700

    append trailing slashes in react router
    fixes GH-2028

--- src/sentry/static/sentry/app/routes.jsx
+++ src/sentry/static/sentry/app/routes.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Router from "react-router";
 var Route = Router.Route;
+var Redirect = Router.Redirect;
 var DefaultRoute = Router.DefaultRoute;
 
 import App from "./views/app";
@@ -28,31 +29,48 @@ import Stream from "./views/stream";
 
 var routes = (
   <Route name="app" path="/" handler={App}>
+    <Redirect from="/organizations/:orgId" to="/organizations/:orgId/" />
     <Route path="/organizations/:orgId/" handler={OrganizationDetails}>
+      <Redirect from="stats" to="/organizations/:orgId/stats/" />
       <Route name="organizationStats" path="stats/" handler={OrganizationStats} />
     </Route>
     <Route name="sharedGroupDetails" path="/share/group/:shareId/" handler={SharedGroupDetails} />
+    <Redirect from="/:orgId" to="/:orgId/" />
     <Route name="organizationDetails" path="/:orgId/" handler={OrganizationDetails}>
       <DefaultRoute name="organizationTeams" handler={OrganizationTeams} />
+      <Redirect from=":projectId" to="/:orgId/:projectId/" />
       <Route name="projectDetails" path=":projectId/" handler={ProjectDetails}>
         <DefaultRoute name="stream" handler={Stream} />
+        <Redirect from="dashboard" to="/:orgId/:projectId/dashboard/" />
         <Route name="projectDashboard" path="dashboard/" handler={ProjectDashboard} />
+        <Redirect from="events" to="/:orgId/:projectId/events/" />
         <Route name="projectEvents" path="events/" handler={ProjectEvents} />
+        <Redirect from="releases" to="/:orgId/:projectId/releases/" />
         <Route name="projectReleases" path="releases/" handler={ProjectReleases} />
+        <Redirect from="releases/:version" to="/:orgId/:projectId/releases/:version/" />
         <Route name="releaseDetails" path="releases/:version/" handler={ReleaseDetails}>
           <DefaultRoute name="releaseNewEvents" handler={ReleaseNewEvents} />
+          <Redirect from="all-events" to="/:orgId/:projectId/releases/:version/all-events/" />
           <Route name="releaseAllEvents" path="all-events/" handler={ReleaseAllEvents} />
+          <Redirect from="artifacts" to="/:orgId/:projectId/releases/:version/artifacts/" />
           <Route name="releaseArtifacts" path="artifacts/" handler={ReleaseArtifacts} />
         </Route>
+        <Redirect from="group/:groupId" to="/:orgId/:projectId/group/:groupId/" />
         <Route name="groupDetails" path="group/:groupId/" handler={GroupDetails}
                ignoreScrollBehavior>
           <DefaultRoute name="groupOverview" handler={GroupEventDetails} />
 
+          <Redirect from="activity" to="/:orgId/:projectId/group/:groupId/activity/" />
           <Route name="groupActivity" path="activity/" handler={GroupActivity} />
+          <Redirect from="events/:eventId" to="/:orgId/:projectId/group/:groupId/events/:eventId/" />
           <Route name="groupEventDetails" path="events/:eventId/" handler={GroupEventDetails} />
+          <Redirect from="events" to="/:orgId/:projectId/group/:groupId/events/" />
           <Route name="groupEvents" path="events/" handler={GroupEvents} />
+          <Redirect from="tags" to="/:orgId/:projectId/group/:groupId/tags/" />
           <Route name="groupTags" path="tags/" handler={GroupTags} />
+          <Redirect from="tags/:tagKey" to="/:orgId/:projectId/group/:groupId/tags/:tagKey/" />
           <Route name="groupTagValues" path="tags/:tagKey/" handler={GroupTagValues} />
+          <Redirect from="reports" to="/:orgId/:projectId/group/:groupId/reports/" />
           <Route name="groupUserReports" path="reports/" handler={GroupUserReports} />
         </Route>
       </Route>
